2c3a17fa84fa6457eda0e3d49d954e08133f9eac
duckypixel <98136801+duckypixel@users.noreply.github.com>
2024-01-11 21:27:10 +0700

    Write main logic

---
+++ main.py
@@ -0,0 +1,31 @@
+import asyncio
+import pyautogui
+import time
+
+async def hitting():
+    while True:
+        for i in range(0,5):
+            await asyncio.sleep(1)
+            pyautogui.keyDown("space")
+            pyautogui.click()
+            pyautogui.keyUp("space")
+        await asyncio.sleep(15)
+
+async def eating():
+    while True: 
+        await asyncio.sleep(180)
+        pyautogui.press("2")
+        pyautogui.mouseDown(button='right')
+        await asyncio.sleep(15)
+        pyautogui.mouseUp(button='right')
+        pyautogui.press("1")
+
+# Run both tasks concurrently
+async def main():
+    task1 = asyncio.create_task(hitting())
+    task2 = asyncio.create_task(eating())
+
+    await task1
+    await task2
+
+asyncio.run(main())
